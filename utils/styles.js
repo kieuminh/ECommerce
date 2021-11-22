367c36fe61d7dbdfee08445999ba6ddeb24a4ca9
Thong.Nguyen <thong.nguyen@aeris.net>
2021-11-22 16:55:19 +0700

    first commit

---
+++ utils/styles.js
@@ -0,0 +1,31 @@
+import { makeStyles } from '@material-ui/core'
+
+const useStyles = makeStyles({
+    navbar: {
+        backgroundColor: '#203040',
+        '& a': {
+            color: '#FFFFFF',
+            marginLeft: 10,
+        }
+    },
+    brand: {
+        fontWeight: 'bold',
+        fontSize: '1.5rem',
+    },
+    grow:{
+        flexGrow: 1,
+    },
+    main: {
+        minHeight: '80vh',
+    },
+    footer: {
+        textAlign: 'center',
+        marginTop: 40,
+    },
+    section: {
+        marginTop: 20,
+        marginBOttom: 20,
+    }
+})
+
+export default useStyles;
